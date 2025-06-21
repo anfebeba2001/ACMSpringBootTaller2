d66eadaed9dc926c6d2d826eb199b69f6208cd62
Andrés <anfebeba2001@gmail.com>
2025-06-20 20:46:24 -0500

    init

---
+++ ReadMe.md
@@ -0,0 +1,12 @@
+1. Preajustes
+
+1.1 Montar db en docker con postgres
+
+1.1.1 aseguramos la presencia de Docker
+
+    docker --version
+    docker-compose --version
+
+1.1.2 levantar el contenedor de la base de datos 
+
+    docker-compose up -d
---
+++ src/main/java/com/ACM/hoteles/HotelesApplication.java
@@ -0,0 +1,13 @@
+package com.ACM.hoteles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelesApplication.class, args);
+	}
+
+}
